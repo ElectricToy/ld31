5b4e0f02097d822720718f758f46d1f3406c9511
Jeff Wofford <github@jeffwofford.com>
2014-12-06 11:35:19 -0600

    Added attachments.

---
+++ src/ldWorld.cpp
@@ -0,0 +1,22 @@
+//
+//  ldWorld.cpp
+//  FreshApp
+//
+//  Created by Jeff Wofford on 12/6/14.
+//  Copyright (c) 2014 Jeff Wofford. All rights reserved.
+//
+
+#include "ldWorld.h"
+#include "Camera.h"
+using namespace fr;
+
+namespace ld
+{	
+	FRESH_DEFINE_CLASS( ldWorld )
+	
+	FRESH_IMPLEMENT_STANDARD_CONSTRUCTORS( ldWorld )
+	
+	// TODO
+	
+}
+
---
+++ src/ldWorld.h
@@ -0,0 +1,31 @@
+//
+//  ldWorld.h
+//  FreshApp
+//
+//  Created by Jeff Wofford on 12/6/14.
+//  Copyright (c) 2014 Jeff Wofford. All rights reserved.
+//
+
+#ifndef FreshApp_ldWorld_h
+#define FreshApp_ldWorld_h
+
+#include "Essentials.h"
+#include "World.h"
+
+namespace ld
+{
+	
+	class ldWorld : public fr::World
+	{
+		FRESH_DECLARE_CLASS( ldWorld, World );
+	public:
+		
+		// TODO
+		
+	private:
+		
+	};
+	
+}
+
+#endif
